afbe7e9eb960a4c2f8943ad383b14e39de4c271a
kabiwabi <91104747+kabiwabi@users.noreply.github.com>
2023-09-18 16:33:38 -0400

    Added unit test for functions

---
+++ test_article_pipeline.py
@@ -0,0 +1,28 @@
+import unittest
+from unittest.mock import patch, mock_open
+from article_pipeline import read_extract_text, tokenize, to_lowercase, stem_tokens, remove_stopwords, \
+    read_stopwords_from_file
+
+
+class PipelineUnitTests(unittest.TestCase):
+
+    def test_tokenize(self):
+        articles = ["Foo Bar Foo Bar", "Bar Foo Bar Foo"]
+        tokenized = tokenize(articles)
+        self.assertEqual(tokenized, [["Foo", "Bar", "Foo", "Bar"], ["Bar", "Foo", "Bar", "Foo"]])
+
+    def test_to_lowercase(self):
+        tokenized_articles = ["FOO", "BAR"]
+        lowercase = to_lowercase(tokenized_articles)
+        self.assertEqual(lowercase, ["foo", "bar"])
+
+    def test_stem_tokens(self):
+        lowercase_tokenized_articles = ["jumping", "flying"]
+        stemmed = stem_tokens(lowercase_tokenized_articles)
+        self.assertEqual(stemmed, ["jump", "fli"])
+
+    def test_remove_stopwords(self):
+        stemmed_tokens = ["the", "foo", "bar", "the", "it"]
+        stopwords_list = ["the"]
+        stopwords_removed = remove_stopwords(stemmed_tokens, stopwords_list)
+        self.assertEqual(stopwords_removed, ["foo", "bar", "it"])
